2a6a21c12459847b6639ddad80a17b2fa83c9690
Jonas Collberg <jonas.collberg@gmail.com>
2025-04-27 17:03:01 +0200

    Add parsing and encoding of Bencode lists

--- src/Bencode/Parser.hs
+++ src/Bencode/Parser.hs
@@ -2,7 +2,7 @@ module Bencode.Parser
   ( parseBencode
   ) where
 
-import Control.Applicative ((<|>))
+import Control.Applicative ((<|>), many)
 import Control.Monad (void)
 import Data.Attoparsec.ByteString
 import Data.Attoparsec.ByteString.Char8
@@ -11,7 +11,7 @@ import Data.Text qualified as T
 import Bencode.Types
 
 parseBencode :: Parser Bencode
-parseBencode = bInt <|> bString
+parseBencode = bInt <|> bString <|> bList
 
 bInt :: Parser Bencode
 bInt = do
@@ -26,3 +26,10 @@ bString = do
   void $ char ':'
   str <- T.pack <$> count n anyChar
   pure $ BString str
+
+bList :: Parser Bencode
+bList = do
+  void $ char 'l'
+  xs <- many parseBencode
+  void $ char 'e'
+  pure $ BList xs
--- src/Bencode/Types.hs
+++ src/Bencode/Types.hs
@@ -4,14 +4,16 @@ module Bencode.Types
 
 import Data.Aeson qualified as A
 import Data.Text (Text)
+import Data.Vector qualified as V
 
 data Bencode
   = BString Text
   | BInt Int
-  | BArray
+  | BList [Bencode]
   | BDict
   deriving (Show)
 
 instance A.ToJSON Bencode where
   toJSON (BString s) = A.String s
   toJSON (BInt n) = A.Number $ fromIntegral n
+  toJSON (BList xs) = A.Array . V.fromList $ map A.toJSON xs
